b9ab1af73fb220c165c4d3fd6eab183f93a8db8a
James Alberto Valencia Caicedo <javalenciacai@gmail.com>
2021-04-05 17:46:00 -0500

    test

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+
+module.exports = {
+    parser: 'babel-eslint',
+    parserOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module',
+      ecmaFeatures: {
+        modules: true,
+        experimentalObjectRestSpread: true
+      }
+    },
+    env: {
+      mocha: true
+    },
+    extends: ['eslint-config-airbnb-base', 'eslint-config-prettier'],
+    rules: {
+      'comma-dangle': ['error', 'never'],
+      'prefer-const': [
+        'error',
+        {
+          destructuring: 'any',
+          ignoreReadBeforeAssign: false
+        }
+      ],
+      'no-var': ['error'],
+      'arrow-parens': [2, 'as-needed'],
+      'prefer-arrow-callback': ['error']
+    }
+  };
