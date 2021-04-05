2619cfdb709fc03e5412082324f63a908d3f8acf
James Alberto Valencia Caicedo <javalenciacai@gmail.com>
2021-04-05 17:47:55 -0500

    test

--- src/index.js
+++
@@ -1,5 +0,0 @@
-const configParser = require('./config-parser');
-const mockMiddleware = require('./mock-middleware');
-
-exports.configParser = configParser;
-exports.mockMiddleware = mockMiddleware;
